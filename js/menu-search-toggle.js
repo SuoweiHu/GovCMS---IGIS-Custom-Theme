fe5191caaed8b3c1e3a7a2138b9bede0f0ebbc78
Suowei Hu <suowei.h@gmail.com>
2023-12-19 14:41:51 +1100

    Modi: fixing main menu search bar missing background issue

--- js/menu-search-toggle.js
+++ js/menu-search-toggle.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var link = document.querySelector('a[href="#block-igis-searchform"]');
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        var search = document.querySelector('#block-igis-searchform');
-        search.classList.toggle('show');
+    var links = document.querySelectorAll('a[href="#block-igis-searchform"]');
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            var search = document.querySelector('#block-igis-searchform');
+            search.classList.toggle('show');
+        });
     });
   });
